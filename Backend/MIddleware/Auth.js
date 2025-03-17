15c006be5d9c44f3fe22f1ca74b679b08d8eb094
Abhradip Roy <abhra0312@gmail.com>
2025-03-18 02:02:34 +0530

    added Learner routes - [signup,login,allcourse,mycourse,buycourse,confirmPurchase]

--- Backend/MIddleware/Auth.js
+++ Backend/MIddleware/Auth.js
@@ -4,7 +4,11 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET
 
 export const auth = (req,res,next)=>{
-    const token = req.headers.token
+    const auth_header = req.headers.Authorization
+    let token
+    if(auth_header){
+        token = auth_header.split(" ")[1] 
+    }
     const decoded = jwt.verify(token,JWT_SECRET)
     if(decoded){
     req.user_id = decoded.id
--- Backend/Routes/Learner.js
+++ Backend/Routes/Learner.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import { Router } from "express";
 import { Learnermodel, TransactionModel,Course_Detail_Model} from "../Db.js";
 import dotenv from "dotenv";
@@ -12,6 +12,15 @@ console.log("salt", typeof saltrounds);
 const router = Router();
 router.use(express.json());
 const JWT_SECRET = process.env.JWT_SECRET;
+
+
+
+
+
+
+
+
+
 
 router.post("/signup", async (req, res) => {
   const { username, email, password, phoneno } = req.body;
@@ -38,6 +47,13 @@ router.post("/signup", async (req, res) => {
     res.status(500).json({ msg: "Internal server error" });
   }
 });
+
+
+
+
+
+
+
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -78,28 +94,97 @@ router.post("/login", async (req, res) => {
   }
 });
 
+
+
+
+router.get("/allcourses",async(req,res)=>{
+    try{
+        const allcourse = await Course_Detail_Model.find()
+        res.status(200).json(allcourse)
+    }catch(err){
+        console.log(err);
+        res.status(500).json({
+            msg:"Internal server error"
+        })
+    }
+})
+
+
+
+
+
 router.get("/mycourse", auth, async (req, res) => {
   const user_id = req.userid;
   let course_id_list = [];
   try {
     const temp = await TransactionModel.find({ user_id: user_id });
+
     if (temp) {
       temp.forEach((e) => {
         course_id_list.push(e.course_id);
       });
     }
-    
     const courses = await Course_Detail_Model.find({ course_id: { $in: course_id_list } });
+    console.log(courses);
     if(courses){
-        res.status(200).json({
-            msg:"Couse details are now availiable"
-        })
+        res.status(200).json(courses)
+    }else{
+        res.status(500).json({msg:"Something went wrong"})
     }
-    console.log(results);
   } catch(e){
     console.log(e);
     res.status(500).json({ msg: "Internal server error" });
   }
 });
+
+
+
+router.get("/buycourse",auth,async(req,res)=>{
+  const course_id = query.course_id
+  const user_id = req.user_id
+  const already_bought = await TransactionModel({course_id:course_id,user_id:user_id})
+  if(already_bought){
+    return res.status(200).json({
+      msg:"You have already bought this course"
+    })
+  }else{
+    const result = await Course_Detail_Model.findOne({course_id:course_id})
+    if(!result){
+      res.status(500).json({
+        msg:"Internal server error!"
+      })
+    }
+    res.status(200).json(result)
+  }
+})
+
+
+
+
+router.post("/confirmPurchase",auth,async(req,res)=>{
+  const user_id = req.user_id
+  const course_id = req.body.course_id
+  const course_name = req.body.course_name
+  const price = req.body.price
+
+
+  try{
+   await TransactionModel.create({
+    user_id:user_id,
+    course_name:course_name,
+    course_id:course_id,
+    Price:price
+   })
+   res.status(200).json({msg:"Course purchased!"})
+  }catch(err){
+    console.log();
+    res.status(400).json({msg:"transaction failed"})
+  }
+})
+
+
+
+
+
 
 export default router;
